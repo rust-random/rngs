cc0478fe01bffbc7f33e04c148280c3506cbf801
tertu marybig <flameshadowxeroshin@gmail.com>
2025-06-21 11:27:53 -0500

    Update copyright info

--- rand_sfc/src/sfc32.rs
+++ rand_sfc/src/sfc32.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 XXX.
+// Copyright 2025 Developers of the Rand project.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
