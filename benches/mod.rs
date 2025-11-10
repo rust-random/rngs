14cc0a1504b1e793200d1accd9fd13fdd32f302f
Diggory Hardy <git@dhardy.name>
2025-11-10 15:30:27 +0000

    Seed benches from a master rng; let rand_sfc use rand_core v0.10.0-rc-2 (#75)

--- benches/mod.rs
+++ benches/mod.rs
@@ -35,6 +35,8 @@ fn gen_bytes(c: &mut Criterion<CyclesPerByte>) {
     let mut g = c.benchmark_group("gen_bytes");
     g.throughput(Throughput::Bytes(BYTES_LEN as u64 * RAND_BENCH_N));
 
+    let mut master = Hc128Rng::seed_from_u64(0);
+
     macro_rules! gen_bytes {
         ($fnn:expr, $gen:expr) => {
             g.bench_function($fnn, |b| {
@@ -50,23 +52,41 @@ fn gen_bytes(c: &mut Criterion<CyclesPerByte>) {
         };
     }
 
-    gen_bytes!("xorshift", XorShiftRng::from_os_rng());
-    gen_bytes!("xoshiro256starstar", Xoshiro256StarStar::from_os_rng());
-    gen_bytes!("xoshiro256plus", Xoshiro256Plus::from_os_rng());
-    gen_bytes!("xoshiro256plusplus", Xoshiro256PlusPlus::from_os_rng());
-    gen_bytes!("xoshiro128starstar", Xoshiro128StarStar::from_os_rng());
-    gen_bytes!("xoshiro128plus", Xoshiro128Plus::from_os_rng());
-    gen_bytes!("xoshiro128plusplus", Xoshiro128PlusPlus::from_os_rng());
-    gen_bytes!("xoroshiro128starstar", Xoroshiro128StarStar::from_os_rng());
-    gen_bytes!("xoroshiro128plus", Xoroshiro128Plus::from_os_rng());
-    gen_bytes!("xoroshiro64starstar", Xoroshiro64StarStar::from_os_rng());
-    gen_bytes!("xoroshiro64star", Xoroshiro64Star::from_os_rng());
-    gen_bytes!("splitmix64", SplitMix64::from_os_rng());
-    gen_bytes!("hc128", Hc128Rng::from_os_rng());
-    gen_bytes!("isaac", IsaacRng::from_os_rng());
-    gen_bytes!("isaac64", Isaac64Rng::from_os_rng());
-    gen_bytes!("sfc32", Sfc32::from_os_rng());
-    gen_bytes!("sfc64", Sfc64::from_os_rng());
+    gen_bytes!("xorshift", XorShiftRng::from_rng(&mut master));
+    gen_bytes!(
+        "xoshiro256starstar",
+        Xoshiro256StarStar::from_rng(&mut master)
+    );
+    gen_bytes!("xoshiro256plus", Xoshiro256Plus::from_rng(&mut master));
+    gen_bytes!(
+        "xoshiro256plusplus",
+        Xoshiro256PlusPlus::from_rng(&mut master)
+    );
+    gen_bytes!(
+        "xoshiro128starstar",
+        Xoshiro128StarStar::from_rng(&mut master)
+    );
+    gen_bytes!("xoshiro128plus", Xoshiro128Plus::from_rng(&mut master));
+    gen_bytes!(
+        "xoshiro128plusplus",
+        Xoshiro128PlusPlus::from_rng(&mut master)
+    );
+    gen_bytes!(
+        "xoroshiro128starstar",
+        Xoroshiro128StarStar::from_rng(&mut master)
+    );
+    gen_bytes!("xoroshiro128plus", Xoroshiro128Plus::from_rng(&mut master));
+    gen_bytes!(
+        "xoroshiro64starstar",
+        Xoroshiro64StarStar::from_rng(&mut master)
+    );
+    gen_bytes!("xoroshiro64star", Xoroshiro64Star::from_rng(&mut master));
+    gen_bytes!("splitmix64", SplitMix64::from_rng(&mut master));
+    gen_bytes!("hc128", Hc128Rng::from_rng(&mut master));
+    gen_bytes!("isaac", IsaacRng::from_rng(&mut master));
+    gen_bytes!("isaac64", Isaac64Rng::from_rng(&mut master));
+    gen_bytes!("sfc32", Sfc32::from_rng(&mut master));
+    gen_bytes!("sfc64", Sfc64::from_rng(&mut master));
 }
 
 // Save a dependency on Rand:
@@ -89,6 +109,8 @@ impl Generate for u64 {
 }
 
 fn gen_uint(c: &mut Criterion<CyclesPerByte>) {
+    let mut master = Hc128Rng::seed_from_u64(1);
+
     macro_rules! gen_uint {
         ($g:expr, $fnn:expr, $ty:ty, $gen:expr) => {
             $g.bench_function($fnn, |b| {
@@ -108,116 +130,157 @@ fn gen_uint(c: &mut Criterion<CyclesPerByte>) {
         let mut g = c.benchmark_group("gen_u32");
         g.throughput(Throughput::Bytes(size_of::<u32>() as u64 * RAND_BENCH_N));
 
-        gen_uint!(g, "xorshift", u32, XorShiftRng::from_os_rng());
+        gen_uint!(g, "xorshift", u32, XorShiftRng::from_rng(&mut master));
         gen_uint!(
             g,
             "xoshiro256starstar",
             u32,
-            Xoshiro256StarStar::from_os_rng()
+            Xoshiro256StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoshiro256plus", u32, Xoshiro256Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoshiro256plus",
+            u32,
+            Xoshiro256Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoshiro256plusplus",
             u32,
-            Xoshiro256PlusPlus::from_os_rng()
+            Xoshiro256PlusPlus::from_rng(&mut master)
         );
         gen_uint!(
             g,
             "xoshiro128starstar",
             u32,
-            Xoshiro128StarStar::from_os_rng()
+            Xoshiro128StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoshiro128plus", u32, Xoshiro128Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoshiro128plus",
+            u32,
+            Xoshiro128Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoshiro128plusplus",
             u32,
-            Xoshiro128PlusPlus::from_os_rng()
+            Xoshiro128PlusPlus::from_rng(&mut master)
         );
         gen_uint!(
             g,
             "xoroshiro128starstar",
             u32,
-            Xoroshiro128StarStar::from_os_rng()
+            Xoroshiro128StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoroshiro128plus", u32, Xoroshiro128Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoroshiro128plus",
+            u32,
+            Xoroshiro128Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoroshiro64starstar",
             u32,
-            Xoroshiro64StarStar::from_os_rng()
+            Xoroshiro64StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoroshiro64star", u32, Xoroshiro64Star::from_os_rng());
-        gen_uint!(g, "splitmix64", u32, SplitMix64::from_os_rng());
-        gen_uint!(g, "hc128", u32, Hc128Rng::from_os_rng());
-        gen_uint!(g, "isaac", u32, IsaacRng::from_os_rng());
-        gen_uint!(g, "isaac64", u32, Isaac64Rng::from_os_rng());
-        gen_uint!(g, "sfc32", u32, Sfc32::from_os_rng());
-        gen_uint!(g, "sfc64", u32, Sfc64::from_os_rng());
+        gen_uint!(
+            g,
+            "xoroshiro64star",
+            u32,
+            Xoroshiro64Star::from_rng(&mut master)
+        );
+        gen_uint!(g, "splitmix64", u32, SplitMix64::from_rng(&mut master));
+        gen_uint!(g, "hc128", u32, Hc128Rng::from_rng(&mut master));
+        gen_uint!(g, "isaac", u32, IsaacRng::from_rng(&mut master));
+        gen_uint!(g, "isaac64", u32, Isaac64Rng::from_rng(&mut master));
+        gen_uint!(g, "sfc32", u32, Sfc32::from_rng(&mut master));
+        gen_uint!(g, "sfc64", u32, Sfc64::from_rng(&mut master));
     }
 
     {
         let mut g = c.benchmark_group("gen_u64");
         g.throughput(Throughput::Bytes(size_of::<u64>() as u64 * RAND_BENCH_N));
 
-        gen_uint!(g, "xorshift", u64, XorShiftRng::from_os_rng());
+        gen_uint!(g, "xorshift", u64, XorShiftRng::from_rng(&mut master));
         gen_uint!(
             g,
             "xoshiro256starstar",
             u64,
-            Xoshiro256StarStar::from_os_rng()
+            Xoshiro256StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoshiro256plus", u64, Xoshiro256Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoshiro256plus",
+            u64,
+            Xoshiro256Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoshiro256plusplus",
             u64,
-            Xoshiro256PlusPlus::from_os_rng()
+            Xoshiro256PlusPlus::from_rng(&mut master)
         );
         gen_uint!(
             g,
             "xoshiro128starstar",
             u64,
-            Xoshiro128StarStar::from_os_rng()
+            Xoshiro128StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoshiro128plus", u64, Xoshiro128Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoshiro128plus",
+            u64,
+            Xoshiro128Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoshiro128plusplus",
             u64,
-            Xoshiro128PlusPlus::from_os_rng()
+            Xoshiro128PlusPlus::from_rng(&mut master)
         );
         gen_uint!(
             g,
             "xoroshiro128starstar",
             u64,
-            Xoroshiro128StarStar::from_os_rng()
+            Xoroshiro128StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoroshiro128plus", u64, Xoroshiro128Plus::from_os_rng());
+        gen_uint!(
+            g,
+            "xoroshiro128plus",
+            u64,
+            Xoroshiro128Plus::from_rng(&mut master)
+        );
         gen_uint!(
             g,
             "xoroshiro64starstar",
             u64,
-            Xoroshiro64StarStar::from_os_rng()
+            Xoroshiro64StarStar::from_rng(&mut master)
         );
-        gen_uint!(g, "xoroshiro64star", u64, Xoroshiro64Star::from_os_rng());
-        gen_uint!(g, "splitmix64", u64, SplitMix64::from_os_rng());
-        gen_uint!(g, "hc128", u64, Hc128Rng::from_os_rng());
-        gen_uint!(g, "isaac", u64, IsaacRng::from_os_rng());
-        gen_uint!(g, "isaac64", u64, Isaac64Rng::from_os_rng());
-        gen_uint!(g, "sfc32", u64, Sfc32::from_os_rng());
-        gen_uint!(g, "sfc64", u64, Sfc64::from_os_rng());
+        gen_uint!(
+            g,
+            "xoroshiro64star",
+            u64,
+            Xoroshiro64Star::from_rng(&mut master)
+        );
+        gen_uint!(g, "splitmix64", u64, SplitMix64::from_rng(&mut master));
+        gen_uint!(g, "hc128", u64, Hc128Rng::from_rng(&mut master));
+        gen_uint!(g, "isaac", u64, IsaacRng::from_rng(&mut master));
+        gen_uint!(g, "isaac64", u64, Isaac64Rng::from_rng(&mut master));
+        gen_uint!(g, "sfc32", u64, Sfc32::from_rng(&mut master));
+        gen_uint!(g, "sfc64", u64, Sfc64::from_rng(&mut master));
     }
 }
 
 fn init(c: &mut Criterion) {
     let mut g = c.benchmark_group("init");
+    let mut master = Hc128Rng::seed_from_u64(2);
 
     macro_rules! init_gen {
         ($fnn:expr, $gen:ident) => {
             g.bench_function($fnn, |b| {
-                let mut rng = XorShiftRng::from_os_rng();
+                let mut rng = XorShiftRng::from_rng(&mut master);
                 b.iter(|| $gen::from_rng(black_box(&mut rng)))
             });
         };
